7e65efbcecba1bc6d48c8201bd8e205d1f3817d5
jesusclemente <clementej@globalhitss.com>
2025-02-25 13:47:21 -0600

    Saving pending syncs

--- core/database/src/main/java/com/jesushzc/core/database/RunDatabase.kt
+++ core/database/src/main/java/com/jesushzc/core/database/RunDatabase.kt
@@ -3,14 +3,22 @@ package com.jesushzc.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.jesushzc.core.database.dao.RunDao
+import com.jesushzc.core.database.dao.RunPendingSyncDao
+import com.jesushzc.core.database.entity.DeletedRunSyncEntity
 import com.jesushzc.core.database.entity.RunEntity
+import com.jesushzc.core.database.entity.RunPendingSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [
+        RunEntity::class,
+        RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class
+    ],
     version = 1
 )
 abstract class RunDatabase: RoomDatabase() {
 
     abstract val runDao: RunDao
+    abstract val runPendingSyncDao: RunPendingSyncDao
 
 }
