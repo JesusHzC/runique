128d0fad921463f8d52cbdad40985c9c4887da1c
jesusclemente <clementej@globalhitss.com>
2025-02-26 17:39:24 -0600

    Setup Theme for Wear app

--- wear/app/src/main/java/com/jesushz/wear/app/presentation/MainActivity.kt
+++ wear/app/src/main/java/com/jesushz/wear/app/presentation/MainActivity.kt
@@ -3,7 +3,14 @@ package com.jesushz.wear.app.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.wear.compose.material3.Button
+import androidx.wear.compose.material3.Text
+import com.jesushz.core.presentation.designsystem_wear.RuniqueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +19,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            RuniqueTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = { }
+                    ) {
+                        Text(
+                            text = "Hello from Wear!",
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                        )
+                    }
+                }
+            }
         }
     }
 }
